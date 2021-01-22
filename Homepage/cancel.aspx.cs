82ea327c06175a42666f619d5b773b47011523da
Sourav <sourav15698@gmail.com>
2021-01-23 00:16:25 +0530

    this is my frst commit

---
+++ Homepage/cancel.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+public partial class Homepage_cancel : System.Web.UI.Page
+{
+    SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\sourav\Downloads\RushBrush(1)\RushBrush\App_Data\Database.mdf;Integrated Security=True");
+    SqlDataAdapter da = new SqlDataAdapter();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Label1.Text = "";
+        Label2.Text = "";
+        Label3.Text = "";
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {        
+        if (TextBox2.Text.Trim() == string.Empty)
+        {
+            MessageBox.Show("Please enter your name");
+            return;
+        }
+        if (TextBox3.Text.Trim() == string.Empty)
+        {
+            MessageBox.Show("Please enter your contact number");
+            return;
+        }
+        if (TextBox1.Text.Trim() == string.Empty)
+        {
+            MessageBox.Show("Please enter your booking id");
+            return;
+        }
+        int a =Int32.Parse(TextBox1.Text);
+        con.Open();
+        da.DeleteCommand = new SqlCommand("delete from infor where Id = " + a + " and name = '" + TextBox2.Text + "' and contactno = '" + TextBox3.Text + "'", con);
+        int rows_affected = da.DeleteCommand.ExecuteNonQuery();
+        if (rows_affected == 0)
+        {
+            MessageBox.Show("Information Invalid");
+            return;
+        }
+        else
+            Response.Redirect("~/Homepage/aftercancel.aspx");
+        con.Close();
+
+    }
+}
---
+++ Homepage/confirmation.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+public partial class Homepage_confirmation : System.Web.UI.Page
+{
+    SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\sourav\Downloads\RushBrush(1)\RushBrush\App_Data\Database.mdf;Integrated Security=True");
+    SqlDataAdapter da = new SqlDataAdapter();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string str1 = Session["new"].ToString();
+        da.SelectCommand = new SqlCommand("select Id from infor where datetime= '" + str1 + "'", con);
+        con.Open();
+        DataSet ds = new DataSet();
+        da.Fill(ds, "inf");
+        con.Close();
+        Label1.Text = ds.Tables[0].Rows[0].ItemArray.GetValue(0).ToString();
+    }
+}
